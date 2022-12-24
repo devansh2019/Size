c31ef5d24efefbc85c0be61618bffccd7a7265d9
devansh2019 <120324041+devansh2019@users.noreply.github.com>
2022-12-24 22:55:43 +0530

    Update and rename README.md to voter.c

---
+++ voter.c
@@ -0,0 +1,15 @@
+#include<isostream>
+using namespace std;
+int main(){
+   int age,left;
+   cout<<"age?";
+   cin>>age;
+   left=18-age
+   if(age>0){
+         if(age>=18)
+              cout<<"apply for voter id";
+         else
+               cout<<left;}
+    else
+         cout<<"enter positive age";
+        
