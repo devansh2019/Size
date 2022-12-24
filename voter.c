a8af6c10737257c0d6a82525cb7d221fd51095c0
devansh2019 <120324041+devansh2019@users.noreply.github.com>
2022-12-25 00:42:00 +0530

    Update voter.c

--- voter.c
+++ voter.c
@@ -1,10 +1,10 @@
-#include<isostream>
+#include<iostream>
 using namespace std;
 int main(){
    int age,left;
    cout<<"age?";
    cin>>age;
-   left=18-age
+   left=18-age;
    if(age>0){
          if(age>=18)
               cout<<"apply for voter id";
@@ -12,4 +12,10 @@ int main(){
                cout<<left;}
     else
          cout<<"enter positive age";
+  int i,p[5];
+   for(i=0;i<=4;i++){
+         cin>>p[i]; }
+   for(i=0;i<=4;i++){
+         cout<< p[i];}}
+
         
